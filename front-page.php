82d9a7ff5292f4f3fd72fa30e36046cddaaa647e
sadek <bilal2dellys@hotmail.com>
2022-08-12 11:31:11 -0400

    s10-c1 rajout d'image d'article

--- front-page.php
+++ front-page.php
@@ -1,12 +1,15 @@
 <?php
 get_header(); ?>
 <section class="site__main">
-    <h1>Exercice#4 </h1>
+    <h1>Exercice#4</h1>
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>
             <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
             <h3><?php the_field('sous_titre'); ?></h3>
+            <?php if (get_field('image')) { ?>
+                <img src="<?php the_field('image'); ?>" />
+            <?php } ?>
             <p><?php the_field('resume'); ?></p>
         <?php endwhile; ?>
     <?php endif; ?>
