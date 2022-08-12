f01087dbf37f764265890ec7f35f7482f0812c47
sadek <bilal2dellys@hotmail.com>
2022-08-12 11:17:40 -0400

    s10-c1 modification de titre principa et rajout detitre et sous titre et resumé

--- front-page.php
+++ front-page.php
@@ -1,22 +1,14 @@
-<?php get_header(); ?>
-
+<?php
+get_header(); ?>
 <section class="site__main">
-    <h1>Un model Wordpress, pour article a lire</h1>
+    <h1>Exercice#4 </h1>
     <?php
-    if (have_posts()):
-        while(have_posts()) : the_post(); ?>
-            <h2><?php the_title(); ?></h2>
-
-    <p>
-        <?= wp_trim_words(get_the_content(), 20, "...<a href='"
-        .get_the_permalink()
-        ."' class=button>"
-        .substr(get_the_title(), 0, 8)
-        ."</a>") ?>
-    </p>
-
+    if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
+            <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
+            <h3><?php the_field('sous_titre'); ?></h3>
+            <p><?php the_field('resume'); ?></p>
         <?php endwhile; ?>
-    <?php endif; ?>    
+    <?php endif; ?>
 </section>
-
 <?php get_footer(); ?>
