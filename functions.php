91d340c62cb13ba35ede50d7f4ff92e8d0a573a7
sadek <bilal2dellys@hotmail.com>
2022-08-12 11:11:54 -0400

    s10-c1 manipulation de pre_get_post pour afficher les articles de categorie accueil

--- functions.php
+++ functions.php
@@ -22,4 +22,16 @@ function mon_underscore_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'mon_underscore_scripts' );
 
+
+/**
+ * permet de modifier la requete principale de la page d'accueil pour extraire uniquement
+ *  des articles de la categorie accueil
+ * @param : $query represente l'objet WP_Query contenant la requete principale
+ */
+function mon_31w_pre_get_posts_acceuil( $query ) {
+	if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
+		$query->set( 'category_name', 'accueil' );
+	}
+}
+add_action( 'pre_get_posts', 'mon_31w_pre_get_posts_acceuil' );
 ?>
